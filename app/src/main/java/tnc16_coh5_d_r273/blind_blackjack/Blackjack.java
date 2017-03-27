d1a28db8845339d6d2ecef99f264d974ed946f77
Darren Rambaud <dtrambaud@txstate.edu>
2017-03-27 02:04:35 -0500

    Starting UI development

--- app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Blackjack.java
+++ app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Blackjack.java
@@ -3,12 +3,32 @@ package tnc16_coh5_d_r273.blind_blackjack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Blackjack extends AppCompatActivity {
+    private Settings settings;
+    private Deck deck;
+    private Purse purse;
+    private BlackJackHand dealer;
+    private Card dealerFaceDown;
+    private Player player;
+    private BlackJackHand currentPlayerHand;
+    private boolean isBetting,
+                    getNextAction;
+    private ArrayList<Integer> splitHands;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        settings = new Settings();
+        purse = new Purse(settings.startCash);
+        deck = new Deck(settings.decks, settings.burns, settings.shuffleTimes);
+        dealer = new BlackJackHand("DEALER");
+        player = new Player("YOU", 1000);
+        currentPlayerHand = player.getHand((byte)0);
+        isBetting = true;
+
         setContentView(R.layout.activity_blackjack);
-        
     }
 }
--- app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Dealer.java
+++ app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Dealer.java
@@ -1,8 +1,11 @@
 package tnc16_coh5_d_r273.blind_blackjack;
 
+import java.util.ArrayList;
+
 /**
  * Created by Valar-Morghulis on 3/21/17.
  */
 
 public class Dealer {
+
 }
