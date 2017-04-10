ed6e30c1079bfc31a4a1f492da16c908aac01ad8
Darren Rambaud <dtrambaud@txstate.edu>
2017-04-09 19:09:57 -0500

    Added Splash Screen

---
+++ app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/MainActivity.java
@@ -0,0 +1,21 @@
+package tnc16_coh5_d_r273.blind_blackjack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blackjack);
+
+        // if onClick Start
+            //begin new game of blackjack
+            // load new thread with betting()
+        // if onClick Tutorial
+            // load tutorial() new thread
+    }
+}
