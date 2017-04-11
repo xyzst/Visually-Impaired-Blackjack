47d8f84f648f6f9fdec7989e0d17a1b74b5822b3
Darren Rambaud <dtrambaud@txstate.edu>
2017-04-11 13:06:50 -0500

    Cleaned up repository, completed GUI implementation, beginning to write code to delay between activities

--- app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Card.java
+++ app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Card.java
@@ -61,15 +61,15 @@ public class Card {
         // If the card's value is invalid, "??" is returned.
         switch ( value ) {
             case 1:   return "Ace";
-            case 2:   return "2";
-            case 3:   return "3";
-            case 4:   return "4";
-            case 5:   return "5";
-            case 6:   return "6";
-            case 7:   return "7";
-            case 8:   return "8";
-            case 9:   return "9";
-            case 10:  return "10";
+            case 2:   return "Two";
+            case 3:   return "Three";
+            case 4:   return "Four";
+            case 5:   return "Five";
+            case 6:   return "Six";
+            case 7:   return "Seven";
+            case 8:   return "Eight";
+            case 9:   return "Nine";
+            case 10:  return "Ten";
             case 11:  return "Jack";
             case 12:  return "Queen";
             case 13:  return "King";
@@ -80,7 +80,7 @@ public class Card {
     public String toString() {
         // Return a String representation of this card, such as
         // "10 of Hearts" or "Queen of Spades".
-        return getValueAsString() + " of " + getSuitAsString();
+        return getValueAsString() + "_of_" + getSuitAsString();
     }
 
 
--- app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Play.java
+++ app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Play.java
@@ -2,6 +2,8 @@ package tnc16_coh5_d_r273.blind_blackjack;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,15 +12,20 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 public class Play extends AppCompatActivity {
     public static final String PREFS_NAME = "SharedBlackjackVars";
     private static final String TAG = "PlayActivity";
 
+    int SECS_DELAY = 5000;
     int bet;
     int dealerScore;
     int userScore;
+    int dealerCardNumber;
+    int userCardNumber;
     double money;
 
     Deck deck;
@@ -31,8 +38,8 @@ public class Play extends AppCompatActivity {
     Button buttonHit;
     Button buttonStand;
 
-    ArrayList<ImageView> dealerCardsImageViews;
-    ArrayList<ImageView> userCardsImageViews;
+    ArrayList<ImageView> dealerCardsImageViews = new ArrayList<>();
+    ArrayList<ImageView> userCardsImageViews = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,18 +57,71 @@ public class Play extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         bet = bundle.getInt("BET_AMOUNT"); // passed from PlaceWagerActivity ..
         money = bundle.getDouble("USER_MONEY");
+        dealerCardNumber = 0;
+        userCardNumber = 0;
+
+        /* Begin initializing ImageViews, 10 refers to the number of ImgViews in the associated
+         * activity_play.xml file. This could change if I could figure out how to dynamically
+         * add ImageViews to the xml file
+         */
+        for (int i = 0; i < 10; ++i) {
+            ImageView imgViewDealer = new ImageView(this);
+            ImageView imgViewUser = new ImageView(this);
+
+            dealerCardsImageViews.add(imgViewDealer);
+            userCardsImageViews.add(imgViewUser);
+
+            dealerCardsImageViews.set(i,
+                    (ImageView) findViewById(getResources().getIdentifier("imageViewDealerCard"+i,
+                            "id", getPackageName())));
+            userCardsImageViews.set(i,
+                    (ImageView) findViewById(getResources().getIdentifier("imageViewUserCard"+i,
+                            "id", getPackageName())));
+        }
 
         shuffleUpAndDeal();
-}
+    }
+
+    private void displayAppropriateUserImageViewCard(String file, int index) {
+        try {
+            String filename = file.toLowerCase();
+            InputStream stream = getAssets().open(filename);
+            Drawable d = Drawable.createFromStream(stream,null);
+            userCardsImageViews.get(index).setImageDrawable(d);
+        } catch (IOException x) {
+            x.printStackTrace();
+        }
+    }
+
+    private void displayAppropriateDealerImageViewCard(String file, int index) {
+        try {
+            String filename = file.toLowerCase();
+            InputStream stream = getAssets().open(filename);
+            Drawable d = Drawable.createFromStream(stream,null);
+            dealerCardsImageViews.get(index).setImageDrawable(d);
+        } catch (IOException x) {
+            x.printStackTrace();
+        }
+    }
 
     public void shuffleUpAndDeal() {
         SharedPreferences pref = getApplicationContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         SharedPreferences.Editor edit = pref.edit();
+
         deck.shuffle();
         dealerHand.addCard(deck.dealCard());
         userHand.addCard(deck.dealCard());
         dealerHand.addCard(deck.dealCard());
         userHand.addCard(deck.dealCard());
+
+        displayAppropriateDealerImageViewCard("face_down.png", dealerCardNumber++);
+        displayAppropriateUserImageViewCard(userHand.getCard(userCardNumber).toString()+".png",
+                userCardNumber++);
+        displayAppropriateDealerImageViewCard(dealerHand.getCard(dealerCardNumber).toString()+".png",
+                dealerCardNumber++);
+        displayAppropriateUserImageViewCard(userHand.getCard(userCardNumber).toString()+".png",
+                userCardNumber++);
+
         dealerScore = dealerHand.getBlackjackValue();
         userScore = userHand.getBlackjackValue();
         dealerScoreTextView.setText("Dealer @ ".concat(Integer.toString(dealerScore)));
@@ -79,7 +139,7 @@ public class Play extends AppCompatActivity {
             startActivity(win);
             //TODO: implement a delay to allow
         }
-        else if (dealerScore == 21 && userScore != 21) {
+        else if (dealerScore == 21) {
             Intent lose = new Intent(this, PlaceWagerActivity.class);
             lose.putExtra("RESULT", money - bet);
             if ((money - bet) == 0) {
@@ -90,14 +150,7 @@ public class Play extends AppCompatActivity {
             startActivity(lose);
         }
 
-        /*  If neither player has Blackjack, play the game.  First the user
-              gets a chance to draw cards (i.e., to "Hit").  The while loop ends
-              when the user chooses to "Stand".  If the user goes over 21,
-              the user loses immediately.
-
-              At this point, you do not need a loop, just draw buttons for hit/stand
-              keep track
-          */
+        /*  If neither player has Blackjack, play the game. */
 
         buttonHit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,7 +158,8 @@ public class Play extends AppCompatActivity {
                 userHand.addCard(deck.dealCard());
                 userScore = userHand.getBlackjackValue();
                 userScoreTextView.setText("You @ ".concat(Integer.toString(userScore)));
-                //Update ImageViewer..
+                displayAppropriateUserImageViewCard(userHand.getCard(userCardNumber).toString()+".png",
+                        userCardNumber++);
                 if ((userScore) > 21) {
                     userHasBusted();
                 }
@@ -134,14 +188,16 @@ public class Play extends AppCompatActivity {
     }
 
     private void userHasPressedStandContinueToDealCardsToDealer() {
+        // TODO: grey out the hit and stand buttons ?? make them inaccessible or just undraw them?
+
         while (dealerHand.getBlackjackValue() <= 16) {
             Card newCard = deck.dealCard();
             dealerHand.addCard(newCard);
             dealerScore = dealerHand.getBlackjackValue();
             dealerScoreTextView.setText("Dealer @ ".concat(Integer.toString(dealerScore)));
             //TODO: implement delay
-            //TODO: implement imageview updates for dealer here
-
+            displayAppropriateDealerImageViewCard(dealerHand.getCard(dealerCardNumber).toString()+".png",
+                    dealerCardNumber++); // FIXME: May access out of bounds if number of cards exceeds 10, fix with dynamic ImageView on XML, use RecyclerView??
             Log.i(TAG, "Dealer Score == "+dealerScore+" User Score == " +userScore);
 
             if (dealerScore > 21) {
@@ -153,16 +209,23 @@ public class Play extends AppCompatActivity {
         }
 
         if (dealerScore == userScore) {
-            Intent tie = new Intent(this, PlaceWagerActivity.class);
-            tie.putExtra("RESULT", money);
-            // TODO implement delay
-            startActivity(tie);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent tie = new Intent(Play.this, PlaceWagerActivity.class);
+                    tie.putExtra("RESULT", money);
+                    // TODO implement delay
+                    startActivity(tie);
+                }
+            }, SECS_DELAY);
         } else if ((dealerScore <= 21) && dealerScore > userScore) {
+
             Log.i(TAG, "Dealer Score == " + dealerScore + " User Score == " + userScore);
             Intent loss = new Intent(this, PlaceWagerActivity.class);
             loss.putExtra("RESULT", money - bet);
             // TODO implement delay
             startActivity(loss);
+            
         } else {
             Intent win = new Intent(this, PlaceWagerActivity.class);
             win.putExtra("RESULT", money + bet);
