50ac8c71bcabf9ef3899249b4c9a4217a42e3541
Darren Rambaud <dtrambaud@txstate.edu>
2017-04-11 15:20:27 -0500

    There existed a problem where floating point fractions would not be represented correctly and thus certain if statements would not execute as expected. The problem has been fixed

--- app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/PlaceWagerActivity.java
+++ app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/PlaceWagerActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -50,11 +51,18 @@ public class PlaceWagerActivity extends AppCompatActivity {
         textViewDisplayFunds.setText("AVAILABLE FUNDS: ".concat(Double.toString(money)));
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            String disp;
+            Integer prog = 5; // set minimum bet value = 5
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                betAmount = i;
-                String x = "CONFIRM ($".concat(betAmount.toString().concat(")"));
-                buttonBetAmount.setText(x);
+                seekBar.setMax((int)money);
+                prog = 5 + i;
+                if (prog > money) {
+                    prog = (int) money;
+                }
+                disp = "CONFIRM ($".concat(prog.toString().concat(")"));
+                buttonBetAmount.setText(disp);
             }
 
             @Override
@@ -64,7 +72,7 @@ public class PlaceWagerActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                betAmount = prog; // Re-adjust after user stops adjusting seekbar to actual amount
             }
         });
     }
--- app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Play.java
+++ app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Play.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,7 +19,7 @@ import java.util.ArrayList;
 
 public class Play extends AppCompatActivity {
     public static final String PREFS_NAME = "SharedBlackjackVars";
-    private static final String TAG = "PlayActivity";
+    public static final String TAG = "PlayActivity";
 
     private final int SECS_DELAY = 5000;
     private int bet;
@@ -86,7 +87,7 @@ public class Play extends AppCompatActivity {
         try {
             String filename = file.toLowerCase();
             InputStream stream = getAssets().open(filename);
-            Drawable d = Drawable.createFromStream(stream,null);
+            Drawable d = Drawable.createFromStream(stream, null);
             userCardsImageViews.get(index).setImageDrawable(d);
         } catch (IOException x) {
             x.printStackTrace();
@@ -97,7 +98,7 @@ public class Play extends AppCompatActivity {
         try {
             String filename = file.toLowerCase();
             InputStream stream = getAssets().open(filename);
-            Drawable d = Drawable.createFromStream(stream,null);
+            Drawable d = Drawable.createFromStream(stream, null);
             dealerCardsImageViews.get(index).setImageDrawable(d);
         } catch (IOException x) {
             x.printStackTrace();
@@ -105,7 +106,8 @@ public class Play extends AppCompatActivity {
     }
 
     public void shuffleUpAndDeal() {
-        final SharedPreferences pref = getApplicationContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        final SharedPreferences pref = getApplicationContext().getSharedPreferences(PREFS_NAME,
+                MODE_PRIVATE);
         final SharedPreferences.Editor edit = pref.edit();
 
         deck.shuffle();
@@ -124,42 +126,48 @@ public class Play extends AppCompatActivity {
 
         dealerScore = dealerHand.getBlackjackValue();
         userScore = userHand.getBlackjackValue();
-        dealerScoreTextView.setText("Dealer @ ".concat(Integer.toString(dealerScore)));
+        dealerScoreTextView.setText("Shown: ".concat(dealerHand.getCard(1).getValueAsString()));
         userScoreTextView.setText("You @ ".concat(Integer.toString(userScore)));
 
         if (dealerScore == 21 && userScore == 21) {
+            //TODO: implement audio feedback
+            Toast.makeText(getApplicationContext(), "Dealer & User have Blackjack! Result = push",
+                    Toast.LENGTH_LONG).show();
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     Intent tie = new Intent(Play.this, PlaceWagerActivity.class);
                     tie.putExtra("RESULT", money);
                     startActivity(tie);
-                    // TODO: implement notification/feedback
                 }
             }, SECS_DELAY);
         }
         else if (dealerScore != 21 && userScore == 21) {
+            //TODO: implement audio feedback
+            Toast.makeText(getApplicationContext(), "User has blackjack! Result = win",
+                    Toast.LENGTH_SHORT).show();
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     Intent win = new Intent(Play.this, PlaceWagerActivity.class);
                     win.putExtra("RESULT", money + (bet * 1.5));
                     startActivity(win);
-                    //TODO: implement feedback/notif
                 }
             }, SECS_DELAY);
         }
         else if (dealerScore == 21) {
+            //TODO: implement audio feedback
+            Toast.makeText(getApplicationContext(), "Dealer has blackjack! Result = loss",
+                    Toast.LENGTH_SHORT).show();
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     Intent lose = new Intent(Play.this, PlaceWagerActivity.class);
                     lose.putExtra("RESULT", money - bet);
-                    if ((money - bet) == 0) {
+                    if (Math.floor((money - bet)) == 0) {
                         edit.putBoolean("hasReachedZeroFunds", true);
                         edit.apply();
                     }
-                    //  TODO: implement feedback notification/audio feedback
                     startActivity(lose);
                 }
             }, SECS_DELAY);
@@ -178,6 +186,9 @@ public class Play extends AppCompatActivity {
                 if ((userScore) > 21) {
                     userHasBusted();
                 }
+                else if (userScore == 21) { // No need to query user to hit, already at 21!
+                    userHasPressedStandContinueToDealCardsToDealer();
+                }
             }
         });
 
@@ -190,81 +201,97 @@ public class Play extends AppCompatActivity {
     }
 
     private void userHasBusted() {
+        final SharedPreferences pref = getApplicationContext().getSharedPreferences(PREFS_NAME,
+                MODE_PRIVATE);
+        final SharedPreferences.Editor edit = pref.edit();
+        //TODO: implement audio feedback
+        Toast.makeText(getApplicationContext(), "User has busted! Result = loss",
+                Toast.LENGTH_SHORT).show();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                SharedPreferences pref = getApplicationContext().getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-                SharedPreferences.Editor edit = pref.edit();
+
                 Intent busted = new Intent(Play.this, PlaceWagerActivity.class);
                 busted.putExtra("RESULT", money - bet);
-                if ((money - bet) == 0) {
+                if (Math.floor((money - bet)) == 0) {
                     edit.putBoolean("hasReachedZeroFunds", true);
                     edit.apply();
                 }
                 startActivity(busted);
-                // TODO: implement audio/notification feedback
             }
         }, SECS_DELAY);
     }
 
     private void userHasPressedStandContinueToDealCardsToDealer() {
         // TODO: grey out the hit and stand buttons ?? make them inaccessible or just undraw them?
+        final SharedPreferences pref = getApplicationContext().getSharedPreferences(PREFS_NAME,
+                MODE_PRIVATE);
+        final SharedPreferences.Editor edit = pref.edit();
 
+        dealerScoreTextView.setText("Dealer @ ".concat(Integer.toString(dealerHand.getBlackjackValue())));
+        displayAppropriateDealerImageViewCard(dealerHand.getCard(0).toString()+".png", 0);
         while (dealerHand.getBlackjackValue() <= 16) {
             Card newCard = deck.dealCard();
             dealerHand.addCard(newCard);
             dealerScore = dealerHand.getBlackjackValue();
             dealerScoreTextView.setText("Dealer @ ".concat(Integer.toString(dealerScore)));
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    displayAppropriateDealerImageViewCard(dealerHand.getCard(dealerCardNumber).toString()+".png",
+            displayAppropriateDealerImageViewCard(dealerHand.getCard(dealerCardNumber).toString()+".png",
                             dealerCardNumber++); // FIXME: May access out of bounds if number of cards exceeds 10, fix with dynamic ImageView on XML, use RecyclerView??
-                }
-            }, (SECS_DELAY / 5)); // 1 second delay between cards
 
             if (dealerScore > 21) {
+                // TODO: implement audio feedback
+                Toast.makeText(getApplicationContext(), "Dealer has busted! Result = win",
+                        Toast.LENGTH_SHORT).show();
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         Intent win = new Intent(Play.this, PlaceWagerActivity.class);
                         win.putExtra("RESULT", money + bet);
                         startActivity(win);
-                        // TODO implement notification
                     }
                 }, SECS_DELAY);
             }
         }
 
         if (dealerScore == userScore) {
+            // TODO: implement sound feedback
+            Toast.makeText(getApplicationContext(), "Dealer hand == User hand! Result == push",
+                    Toast.LENGTH_SHORT).show();
              new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     Intent tie = new Intent(Play.this, PlaceWagerActivity.class);
                     tie.putExtra("RESULT", money);
                     startActivity(tie);
-                    // TODO: implement notification/sound feedback
                 }
             }, SECS_DELAY);
-        } else if ((dealerScore <= 21) && dealerScore > userScore) {
+        } else if ((dealerScore <= 21) && (dealerScore > userScore)) {
+            // TODO: implement sound feedback
+            Toast.makeText(getApplicationContext(), "Dealer hand > User hand! Result == loss",
+                    Toast.LENGTH_SHORT).show();
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     Log.i(TAG, "Dealer Score == " + dealerScore + " User Score == " + userScore);
                     Intent loss = new Intent(Play.this, PlaceWagerActivity.class);
                     loss.putExtra("RESULT", money - bet);
+                    if (Math.floor(money - bet) == 0) {
+                        edit.putBoolean("hasReachedZeroFunds", true);
+                        edit.apply();
+                    }
                     startActivity(loss);
-                    // TODO: implement notification/sound feedback
                 }
             }, SECS_DELAY);
         } else {
+            // TODO implement sound feedback
+            Toast.makeText(getApplicationContext(), "User hand > dealer hand! Result == win",
+                    Toast.LENGTH_SHORT).show();
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
                     Intent win = new Intent(Play.this, PlaceWagerActivity.class);
                     win.putExtra("RESULT", money + bet);
                     startActivity(win);
-                    // TODO: implement notification/sound feedback
                 }
             }, SECS_DELAY);
         }
