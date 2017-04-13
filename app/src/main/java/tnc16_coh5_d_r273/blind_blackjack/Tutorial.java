933379abe2adb3d5bd9b70c84e17603e431520eb
Darren Rambaud <dtrambaud@txstate.edu>
2017-04-12 19:02:23 -0500

    Wrote the documentation for Tutorial class

--- app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Tutorial.java
+++ app/src/main/java/tnc16_coh5_d_r273/blind_blackjack/Tutorial.java
@@ -15,6 +15,5 @@ public class Tutorial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
-        
     }
 }
